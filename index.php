1db966fdf0c99c675d33fcd735622a831553f67e
nejigram <nejigram@gmail.com>
2019-05-22 21:39:09 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0">
+
+<link rel="shortcut icon" type="image/x-icon" href="//www.nejigram.com/assets/img/itemicon.ico" />
+<title>twlike</title>
+<link rel='stylesheet' href='../../css/style.css?<?=time();?>' type='text/css' />
+<link rel='stylesheet' href='css/style.css?<?=time();?>' type='text/css' />
+<script src='js/script.js' async></script>
+</head>
+<body>
+<header>
+    <div class="into">
+        <ul class="menu">
+        <li><img src="../../assets/images/icon/001.png" height="100%"><span>ホーム</span></li>
+        <li><img src="../../assets/images/icon/003.png" height="100%"><span>モーメント</span></li>
+        <li><img src="../../assets/images/icon/019.png" height="100%"><span>通知</span></li>
+        <li><img src="../../assets/images/icon/001.png" height="100%"><span>メッセージ</span></li>
+        </ul>
+        <div class="homeicon">
+            <img src="../../assets/images/icon/158.png">
+        </div>
+        <div class="rightarea">
+            <div class="searchbox">
+                <input type="search" placeholder="キーワード検索" class="searchbox"><span class="searchicon"><img src="../../assets/images/icon/087_1.png"></span>
+            </div>
+            <div class="accounticon">
+                <img src="../../assets/images/animal/01-min.jpg">
+            </div>
+            <button class="tweetbtn">ツイート</button>
+        </div>
+    </div>
+</header>
+<div class="main" id="main">
+    <div class="left">
+        <div class="mydataarea">
+            <div class="img">
+                <img src="../../assets/images/animal/02-min.jpg">
+            </div>
+            <div class="data" id="left_mydataarea_data">
+                <div class="accounticon">
+                    <img src="../../assets/images/animal/01-min.jpg">
+                </div>
+                <div class="name">
+                    aaa
+                </div>
+                <div class="fs_0_5 pl_0_5 mt_1">
+                    <p>まさしきねがいに　いさかうとも</p>
+                    <p>銀河のかなたに　　ともにわらい</p>
+                </div>
+                <div class="flex pt_1 pl_0_5 pb_1">
+                    <div class="w33">
+                        <p class="fs_0_6 lh_1">ツイート</p>
+                        <p class="fs_1 lh_1 bld">111</p>
+                    </div>
+                    <div class="w33">
+                        <p class="fs_0_6 lh_1">ツイート</p>
+                        <p class="fs_1 lh_1 bld">111</p>
+                    </div>
+                    <div class="w33">
+                        <p class="fs_0_6 lh_1">ツイート</p>
+                        <p class="fs_1 lh_1 bld">111</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="center">
+        <div class="tweetline">
+            <div class="accounticon">
+                <img src="../../assets/images/animal/01-min.jpg">
+            </div>
+            <input type="text" placeholder="いまどうしてる？"/>
+        </div>
+
+    center
+    </div>
+    <div class="right">
+    right
+    </div>
+
+</div>
+
+</body>
+<html>
---
+++ js/script.js
@@ -0,0 +1,5 @@
+const header = document.querySelector("header");
+const mainbox = document.querySelector("#main");
+const left_mydataarea_data = document.querySelector("#left_mydataarea_data");
+mainbox.style.marginTop = header.clientHeight + "px";
+//left_mydataarea_data.style.height = 
